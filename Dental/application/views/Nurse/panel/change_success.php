a9d7683a16b15e76adc8538d243e40daa6f0d765
yashanthy <yashanthy93@gmail.com>
2016-10-25 15:09:12 -0500

    added alert message for success registration

---
+++ Dental/application/views/Nurse/panel/change_success.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assest/'); ?>dist/sweetalert.css">
+    <script src="<?php echo base_url('assest/'); ?>dist/sweetalert.min.js"></script>
+</head>
+<body>
+<script language="javascript">
+    swal({  title: "Changed Successfully!",   text: "Updated your username",   type: "success",   confirmButtonText: "ok" });
+</script>
+
+</body>
+</html>
