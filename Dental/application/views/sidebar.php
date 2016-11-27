b66e9b1cdfdd09a4bdc0a3fff2bd35b7746e5e9e
SHP-Hari <greathari777@gmail.com>
2016-11-27 14:59:38 +0530

    qwewe

---
+++ Dental/application/views/sidebar.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<div class="col-md-3">
+            <!-- *** CUSTOMER MENU ***
+        _________________________________________________________ -->
+            <div class="panel panel-default sidebar-menu">
+
+                <div class="panel-heading">
+                    <h3 class="panel-title">Patient Section</h3>
+                </div>
+
+                <div class="panel-body">
+
+                    <ul class="nav nav-pills nav-stacked">
+                        <li class="active">
+                            <a href="customer-orders.html"><i class="fa fa-list"></i> Patient</a>
+                        </li>
+                        <li>
+                            <a href="customer-wishlist.html"><i class="fa fa-heart"></i> Treatments</a>
+                        </li>
+                        <li>
+                            <a href="customer-account.html"><i class="fa fa-user"></i> My account</a>
+                        </li>
+                        <li>
+                            <a href="index.html"><i class="fa fa-sign-out"></i> Logout</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- /.col-md-3 -->
+
+            <!-- *** CUSTOMER MENU END *** -->
+        </div>
